ca1f0f53bbe22964fef63f7c7c85f8f10b42c976
arushiyadav <36760416+arushiyadav@users.noreply.github.com>
2018-03-19 23:10:08 +0530

    Add files via upload

---
+++ str_formatting.py
@@ -0,0 +1,12 @@
+str0 = "Jamesbond007"
+str1 = "Jamesbond"
+str2 = "007"
+str3 = "jamesbond"
+str4 = "ABCD"
+str5 = " "
+print(str0.isalnum())
+print(str1.isalpha())
+print(str2.isdigit())
+print(str3.islower())
+print(str5.isspace())
+print(str4.isupper())
